c8198bd6d875e413ee4d626a5de63977ab2c603b
Mike McMullan <apps@mike-mcmullan.com>
2015-07-07 20:57:24 -0400

    For some reason I was not protecting the bot-control page

--- app/Http/Controllers/BotController.php
+++ app/Http/Controllers/BotController.php
@@ -10,7 +10,7 @@ class BotController extends Controller {
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['botControl']]);
+        $this->middleware('auth');
     }
 
     public function botControl()
