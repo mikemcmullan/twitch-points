28d211c0b3c5de77925d9392c3d4489f1702ccea
Mike McMullan <mike@mikemcmullan.me>
2016-02-04 22:38:13 -0500

    Updated routes.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,11 +18,45 @@ Route::group(['domain' => config('app.api_domain'), 'prefix' => '{channel}', 'na
         'as'    => 'api_vips_path'
     ]);
 
+    /*
+     * Commands Routes
+     */
     Route::get('/commands', [
-        'uses'  => 'CommandsController@getCommands',
+        'uses'  => 'CommandsController@index',
         'as'    => 'api_commands_path'
     ]);
 
+    Route::get('/commands/{id}', [
+        'uses'  => 'CommandsController@show',
+        'as'    => 'api_commands_show_path'
+    ]);
+
+    Route::post('/commands', [
+        'uses' => 'CommandsController@store',
+        'as'   => 'api_commands_store_path'
+    ]);
+
+    Route::put('/commands/{id}', [
+        'uses' => 'CommandsController@update',
+        'as'   => 'api_commands_update_path'
+    ]);
+
+    Route::delete('/commands/{id}', [
+        'uses' => 'CommandsController@destroy',
+        'as'   => 'api_commands_destroy_path'
+    ]);
+
+    /*
+     * Settings Routes
+     */
+    Route::put('/settings', [
+        'uses' => 'SettingsController@update',
+        'as'   => 'api_settings_update_path'
+    ]);
+
+    /*
+     * Currency Routes
+     */
     Route::post('/currency', [
         'uses'  => 'CurrencyController@addCurrency',
         'as'    => 'api_currency_add_path'
@@ -33,45 +67,82 @@ Route::group(['domain' => config('app.api_domain'), 'prefix' => '{channel}', 'na
         'as'    => 'api_currency_remove_path'
     ]);
 
-    Route::get('/giveaway/enter', [
-        'uses'  => 'GiveAwayController@enter',
+    Route::post('/currency/start-system', [
+        'uses'  => 'CurrencyController@startSystem',
+        'as'    => 'api_currency_stop_system_path'
+    ]);
+
+    Route::post('/currency/stop-system', [
+        'uses'  => 'CurrencyController@stopSystem',
+        'as'    => 'api_currency_start_system_path'
+    ]);
+
+    /*
+     * Giveaway Routes
+     */
+    Route::get('/giveaway/entries', [
+        'uses'  => 'GiveawayController@entries',
+        'as'    =>  'api_giveaway_entries_path'
+    ]);
+
+    Route::post('/giveaway/enter', [
+        'uses'  => 'GiveawayController@enter',
         'as'    => 'api_giveaway_enter_path'
     ]);
 
-    Route::get('update-caches', function(\Illuminate\Http\Request $request) {
-        \Event::fire(new \App\Events\CommandWasUpdated($request->route()->getParameter('channel')));
-        \Event::fire(new \App\Events\VIPsWasUpdated($request->route()->getParameter('channel')));
-    });
+    Route::post('/giveaway/start', [
+        'uses'  => 'GiveawayController@start',
+        'as'    => 'api_giveaway_start_path'
+    ]);
+
+    Route::post('/giveaway/stop', [
+        'uses'  => 'GiveawayController@stop',
+        'as'    => 'api_giveaway_stop_path'
+    ]);
+
+    Route::post('/giveaway/reset', [
+        'uses'  => 'GiveawayController@reset',
+        'as'    => 'api_giveaway_reset_path'
+    ]);
+
+    Route::get('/giveaway/winner', [
+        'uses'  => 'GiveawayController@winner',
+        'as'    => 'api_giveaway_winner_path'
+    ]);
 });
 
-
 Route::group(['domain' => '{channel}.' . config('app.root_domain'), 'middleware' => ['web']], function () {
-
     Route::get('/', [
-        'uses'  => 'CurrencyController@checkPoints',
-        'as'    => 'home_path'
+        'uses'  => function(\App\Channel $channel) {
+            return redirect()->route('scoreboard_path', [$channel->slug])->with('message', session('message'));
+        },
+        'as' => 'home_path'
     ]);
 
-    Route::get('/check-points', [
-        'uses'  => 'CurrencyController@checkPoints',
-        'as'    => 'check_points_path'
-    ]);
+    // Route::get('/commands', [
+    //     'uses'  => 'CommandsController@index',
+    //     'as'    => 'commands_path'
+    // ]);
 
-    Route::get('/system-control', [
-        'uses'  => 'CurrencyController@systemControl',
-        'as'    => 'system_control_path'
-    ]);
-
-    Route::patch('/system-control', [
-        'uses'  => 'CurrencyController@startSystem',
-        'as'    => 'start_system_path'
-    ]);
-
+    /**
+     * Currency Routes
+     */
     Route::get('/scoreboard', [
         'uses'  => 'CurrencyController@scoreboard',
         'as'    => 'scoreboard_path'
     ]);
 
+    /**
+     * Giveaway Routes
+     */
+    Route::get('/giveaway', [
+        'uses'  => 'GiveawayController@index',
+        'as'    => 'giveaway_path'
+    ]);
+
+    /**
+     * Authentication Routes
+     */
     Route::get('/login', [
         'uses'  => 'AuthController@login',
         'as'    => 'login_path'
@@ -82,38 +153,8 @@ Route::group(['domain' => '{channel}.' . config('app.root_domain'), 'middleware'
         'as'    => 'logout_path'
     ]);
 
-    Route::get('/giveaway', [
-        'uses'  => 'GiveAwayController@index',
-        'as'    => 'giveaway_path'
-    ]);
-
-    Route::post('/giveaway/reset', [
-        'uses'  => 'GiveAwayController@reset',
-        'as'    => 'giveaway_reset_path'
-    ]);
-
-    Route::post('/giveaway/start', [
-        'uses'  => 'GiveAwayController@start',
-        'as'    => 'giveaway_start_path'
-    ]);
-
-    Route::post('/giveaway/stop', [
-        'uses'  => 'GiveAwayController@stop',
-        'as'    => 'giveaway_stop_path'
-    ]);
-
     Route::post('/pusher/auth', [
         'uses'  => 'AuthController@pusher',
         'as'    => 'pusher_auth_path'
-    ]);
-
-    Route::post('/giveaway/winner', [
-        'uses'  => 'GiveAwayController@winner',
-        'as'    => 'giveaway_winner_path'
-    ]);
-
-    Route::post('/giveaway/save-settings', [
-        'uses'  => 'GiveAwayController@saveSettings',
-        'as'    => 'giveaway_save_settings_path'
     ]);
 });
