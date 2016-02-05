0a803dc51db0c93eb1e82343a0e567559d60e736
Mike McMullan <mike@mikemcmullan.me>
2016-02-04 22:33:22 -0500

    --

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,13 @@
 var elixir = require('laravel-elixir');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Less
- | file for our application, as well as publishing vendor resources.
- |
- */
+require('laravel-elixir-vueify');
 
 elixir.config.css.sass.folder = 'scss';
+elixir.config.js.outputFolder = 'assets/js';
+elixir.config.js.browserify.watchify.options.poll = true;
 
 elixir(function(mix) {
-    mix.sass('style.scss', 'public/assets/css');
+    mix.sass('style.scss', 'public/assets/css')
+        .less('AdminLTE.less', 'public/assets/css')
+        .browserify('admin.js');
 });
