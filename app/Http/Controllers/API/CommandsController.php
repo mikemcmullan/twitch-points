c281faf22101fa95a3ffb5fd665ba3bfda57a735
Mike McMullan <mike@mikemcmullan.me>
2016-02-04 22:52:34 -0500

    API to manage commands.

--- app/Http/Controllers/API/CommandsController.php
+++ app/Http/Controllers/API/CommandsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests;
 use App\Channel;
+use App\Command;
+use App\Exceptions\UnauthorizedRequestException;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use \App\BotCommands\Manager as CommandManager;
 
 class CommandsController extends Controller
 {
@@ -15,29 +20,106 @@ class CommandsController extends Controller
     private $channel;
 
     /*
+     * @var CommandManager
+     */
+    private $commandManager;
+
+    /*
      * @param Request $request
      */
-    public function __construct(Request $request)
+    public function __construct(Request $request, CommandManager $commandManager)
     {
-        // $this->middleware('protect.api');
+        $this->middleware('jwt.auth', ['only' => ['store', 'update', 'destroy']]);
         $this->channel = $request->route()->getParameter('channel');
+        $this->commandManager = $commandManager;
     }
 
     /**
+     * Get a list of commands.
+     *
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function getCommands()
+    public function index(Channel $channel)
     {
-        $results = $this->channel->commands()->with('strings')->get();
+        $commands = $this->commandManager->all($channel);
 
-        return $results->map(function ($command) {
-            $array = array_only($command->toArray(), ['id', 'pattern', 'level', 'type', 'file', 'response']);
+        return response()->json($commands);
+    }
 
-            $command->strings->each(function ($string) use (&$array) {
-                $array['strings'][$string->name] = $string->value;
-            });
+    /**
+     * Display a command.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function show(Channel $channel, $id)
+    {
+        $command = $this->commandManager->get($channel, $id);
 
-            return $array;
-        });
+        return response()->json($command);
+    }
+
+    /**
+     * Create a new command.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function store(Request $request, Channel $channel)
+    {
+        // Pattern, level, response
+        $data = $request->only(['command', 'level', 'response']);
+
+        // Validate the request
+        $validator = \Validator::make($data, [
+            'command' => 'required|min:2|max:80',
+            'level'   => 'in:everyone,mod,admin,owner',
+            'response'=> 'required|min:2|max:400'
+        ]);
+
+        if ($validator->fails()) {
+            throw new BadRequestHttpException(json_encode(['validation_errors' => $validator->errors()->getMessages()]));
+        }
+
+        $response = $this->commandManager->create($channel, $data);
+
+        return response()->json($response);
+    }
+
+    /**
+     * Update a command.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function update(Request $request, Channel $channel, $id)
+    {
+        // Pattern, level, response
+        $data = $request->only(['command', 'level', 'response']);
+        $data = array_map('trim', $data);
+
+        // Validate the request
+        $validator = \Validator::make($data, [
+            'command' => 'min:2|max:80',
+            'level'   => 'in:everyone,mod,admin,owner',
+            'response'=> 'min:2|max:400'
+        ]);
+
+        if ($validator->fails()) {
+            throw new BadRequestHttpException(json_encode(['validation_errors' => $validator->errors()->getMessages()]));
+        }
+
+        $response = $this->commandManager->update($channel, $id, $data);
+
+        return response()->json($response);
+    }
+
+    /**
+     * Delete a command.
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function destroy(Request $request, Channel $channel, $id)
+    {
+        $this->commandManager->delete($channel, $id);
+
+        return response()->json(['ok' => 'success']);
     }
 }
