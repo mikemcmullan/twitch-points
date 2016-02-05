6e4cac9d83ee7848a27ae3d36255a630947d5796
Mike McMullan <mike@mikemcmullan.me>
2016-02-04 22:37:24 -0500

    Simple settings api. Needs work.

---
+++ app/Http/Controllers/API/SettingsController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+
+class SettingsController extends Controller
+{
+    /**
+     *
+     */
+    public function __construct(Request $request)
+    {
+        $this->middleware('jwt.auth');
+        $this->channel = $request->route()->getParameter('channel');
+    }
+
+    /**
+     * Update settings.
+     */
+    public function update(Request $request)
+    {
+        $newSettings = $request->all();
+        $errors = [];
+
+        foreach ($newSettings as $setting => $value) {
+            if ($this->channel->getSetting($setting) === null) {
+                $errors[$setting] = ['invalid_setting'];
+            }
+        }
+
+        if (! empty($errors)) {
+            throw new BadRequestHttpException(json_encode(['validation_errors' => $errors]));
+        }
+
+        $this->channel->setSetting($newSettings);
+
+        return response()->json($newSettings, 200);
+    }
+}
