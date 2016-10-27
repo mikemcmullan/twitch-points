a24f0801a4e010d0f63cbda89b3f244fb442de78
Mike McMullan <mike@mikemcmullan.me>
2016-10-27 16:16:32 -0400

    no message

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
-            'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]
     ];
 
